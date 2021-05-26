b4591467a38fa289379d47550582324600d79b92
Bojan S <76212320+Bojan-Spasovski@users.noreply.github.com>
2021-05-26 17:02:32 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// Your JS code here
+var subTypeElement = document.querySelector("#subscription");
+var subDurationElement = document.querySelector("#months");
+var result = document.querySelector(".result");
+var subType = "basic";
+var subDuration = 1;
+
+subTypeElement.addEventListener("change", function (e) {
+  subType = e.target.value;
+  updateSubscriptionDiv();
+  //console.log(subType);
+});
+
+subDurationElement.addEventListener("change", function (e) {
+  subDuration = Number(e.target.value);
+  updateSubscriptionDiv();
+  //console.log(subDuration);
+});
+
+var updateSubscriptionDiv = function () {
+  var monthlyCost = 5;
+
+  if (subType === "standard") {
+    monthlyCost = 7;
+  } else if (subType === "premium") {
+    monthlyCost = 10;
+  }
+  var total = subDuration * monthlyCost;
+  result.innerText = `"You have chosen a ${subDuration} month 
+  ${subType} plan for $${total}."`;
+};
+
+/* 
+Below the total variable, change the innerText of the result variable. 
+In the value, create a message that will say something like 
+"You have chosen a subDuration month subType plan for $total." 
+Note: You'll use template literal placeholders where the variable 
+names are in the example message.
+*/
